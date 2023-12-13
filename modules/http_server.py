e1b1293587e31d16954dc77e11e75e4328a82496
Michał Matczuk <mm@webx.pl>
2023-12-13 20:16:28 +0100

    feat: change ports

--- modules/http_server.py
+++ modules/http_server.py
@@ -1,3 +1,5 @@
+import os
+
 from aiohttp import web
 
 from config import HOSTNAME
@@ -15,7 +17,7 @@ async def start_http_server():
     # Start the web server
     runner = web.AppRunner(app)
     await runner.setup()
-    site = web.TCPSite(runner, HOSTNAME, 8888)
+    site = web.TCPSite(runner, HOSTNAME, os.getenv("PORT"))
     await site.start()
 
     print("WEB server started:", site._host, site._port)
